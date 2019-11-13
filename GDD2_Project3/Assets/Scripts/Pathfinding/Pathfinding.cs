c468a25637b3fa14c90e50b5c4139f511e7fa676
Nick Rasmussen (RIT Student) <nlr3026@ad.rit.edu>
2019-11-13 10:30:51 -0500

    real basic cat path following

---
+++ GDD2_Project3/Assets/Scripts/Pathfinding/Pathfinding.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour
+{
+    [SerializeField] List<GameObject> path;
+    [SerializeField] float minRangeToArrive = 1.0f;
+    [SerializeField] float speed = 1.0f;
+
+    GameObject target;
+
+    Vector3 position;
+    Vector3 direction;
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+        target = path[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs((target.transform.position - transform.position).sqrMagnitude) < minRangeToArrive * minRangeToArrive)
+        {
+            if(target == path[path.Count - 1])
+                target = path[0];
+            else
+                target = path[path.IndexOf(target) + 1];
+        }
+
+        direction = (target.transform.position - transform.position).normalized;
+        velocity = direction * speed;
+        position += velocity;
+        transform.position = position;
+    }
+}
