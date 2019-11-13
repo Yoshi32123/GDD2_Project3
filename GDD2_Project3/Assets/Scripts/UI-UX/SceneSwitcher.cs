3d8012ef5b60dc0fbf513aae8267bcdc70dc35a5
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2019-11-13 10:43:55 -0500

    Cleaned up more of the scene scripts and turned the options overlay into a prefab.
    Please edit the prefab if you want to change the option scene's layout and stuff

--- GDD2_Project3/Assets/Scripts/UI-UX/SceneSwitcher.cs
+++ GDD2_Project3/Assets/Scripts/UI-UX/SceneSwitcher.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    public string LevelSceneName;
 
     /**<summary>
      * switches to the scene whose name matches the given game.</summary>
