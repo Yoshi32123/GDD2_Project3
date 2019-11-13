d5833347db58d2e303a9b878870e040c5c1b60eb
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2019-11-13 10:38:29 -0500

    Created basic title screen and empty gameplay scene
    Option overlay is non-functional right now. Scripts are added to handle scene switching and overlays

---
+++ GDD2_Project3/Assets/Scripts/UI-UX/MusicFunctionality.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicFunctionality : MonoBehaviour
+{
+    public static bool MusicEnabled = true;
+
+    //sets toggle state
+    void Start()
+    {
+        Toggle toggle = gameObject.GetComponent<Toggle>();
+        toggle.isOn = MusicEnabled;
+    }
+
+    /// <summary>
+    /// Gets value of toggle and sets music state
+    /// </summary>
+    /// <param name="ToggleState">New value of toggle</param>
+    public void OnValueChanged(bool ToggleState)
+    {
+        MusicEnabled = ToggleState;
+    }
+}
---
+++ GDD2_Project3/Assets/Scripts/UI-UX/SoundFunctionality.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundFunctionality : MonoBehaviour
+{
+    public static bool SoundEnabled = true;
+
+    //sets toggle state
+    void Start()
+    {
+        Toggle toggle = gameObject.GetComponent<Toggle>();
+        toggle.isOn = SoundEnabled;
+    }
+
+    /// <summary>
+    /// Gets value of toggle and sets sound state
+    /// </summary>
+    /// <param name="ToggleState">New value of toggle</param>
+    public void OnValueChanged(bool ToggleState)
+    {
+        SoundEnabled = ToggleState;
+    }
+
+}
