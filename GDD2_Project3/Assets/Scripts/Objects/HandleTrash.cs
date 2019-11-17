3f0407433253b478483338396d5b804fcb9d0a79
Kyler McQuillan (RIT Student) <kjm7088@ad.rit.edu>
2019-11-17 14:47:01 -0500

    Particle effect & sound for item in the trash

--- GDD2_Project3/Assets/Scripts/Objects/HandleTrash.cs
+++ GDD2_Project3/Assets/Scripts/Objects/HandleTrash.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class HandleTrash : MonoBehaviour
 {
+    [SerializeField] GameObject effects = null;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Trash")
         {
+            Instantiate(effects, collision.gameObject.transform.position, Quaternion.identity);
             Destroy(collision.gameObject);
         }
     }
