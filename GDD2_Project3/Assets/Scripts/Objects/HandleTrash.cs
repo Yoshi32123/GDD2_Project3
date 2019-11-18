27930e6f0eb70f1feca98c7d5c998f9844d96b2f
Kyler McQuillan <yoshi321233@gmail.com>
2019-11-18 10:52:57 -0500

    Lots of tweaks. Gameplay scene implemented

--- GDD2_Project3/Assets/Scripts/Objects/HandleTrash.cs
+++ GDD2_Project3/Assets/Scripts/Objects/HandleTrash.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class HandleTrash : MonoBehaviour
 {
     [SerializeField] GameObject effects = null;
+    private GameObject effectsTrack = null;
+
+    void Update()
+    {
+        if (effectsTrack != null)
+        {
+            if (effectsTrack.GetComponent<ParticleSystem>().isPlaying == false)
+            {
+                Destroy(effectsTrack);
+            }
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Trash")
         {
-            Instantiate(effects, collision.gameObject.transform.position, Quaternion.identity);
+            effectsTrack = Instantiate(effects, collision.gameObject.transform.position, Quaternion.identity);
             Destroy(collision.gameObject);
         }
     }
