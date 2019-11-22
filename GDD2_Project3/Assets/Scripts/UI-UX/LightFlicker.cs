e152120ca43c41eb0df74177e59ed4ede7986d03
rkb2204 <rkb2204@rit.edu>
2019-11-22 10:57:09 -0500

    LightFlicker got improved
    applied a sinusoidal wave

--- GDD2_Project3/Assets/Scripts/UI-UX/LightFlicker.cs
+++ GDD2_Project3/Assets/Scripts/UI-UX/LightFlicker.cs
@@ -6,13 +6,17 @@ public class LightFlicker : MonoBehaviour
 {
     Light Light;
     bool flickering;
-    int numCheck = 0;
+    float numCheck = 0;
+    bool up = false;
+    float amplitude = 0;
+    float angularFrequency = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
         Light = GetComponent<Light>();
-        StartCoroutine(Flickering());
+        StartCoroutine(Run());
     }
 
     // Update is called once per frame
@@ -20,37 +24,41 @@ public class LightFlicker : MonoBehaviour
     {
         
     }
+    IEnumerator Run()
+    {
+        while (true)
+        {
+            //yield return new WaitForSeconds(Random.Range(1f, 50f));
+            //yield return new WaitForSeconds(.01f);
+
+            StartCoroutine(Flickering());
+        }
+    }
 
     IEnumerator Flickering()
     {
+        flickering = true;
+        amplitude = .2f;
+        angularFrequency = 2 * Mathf.PI * 100;
         while (true)
         {
             // give a time frame for these events
             yield return new WaitForSeconds(.01f);
-
+            numCheck += .01f;
             if (flickering)
             {
-                // increase the intensity of the light back to full
-                Light.intensity += .01f;
-                if (Light.intensity >= .3f)
+                // oscillating as a decaying sinusoidal wave
+                Light.intensity = amplitude * Mathf.Sin(angularFrequency * numCheck) + .3f;
+                amplitude *= -.9f;
+                if (amplitude <= .05f)
                 {
-                    Light.intensity = .3f;
-
-                    // put a timer in here through the waitForSeconds
-                    numCheck++;
-                    if (numCheck >= 150)
-                    {
-                        // reset our timer
-                        numCheck = 0;
-                        flickering = false;
-                    }
+                    flickering = false;
                 }
             }
             else
             {
-                // suddenly set the light intensity lower, to simulate sudden flickering
-                Light.intensity = .1f;
-                flickering = true;
+                // end the flickering
+                //StopCoroutine(Flickering());
             }
         }
         
