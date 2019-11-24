c7b25a335878ed8d9cc03820f5b7952b40b5fa61
Nick Rasmussen (RIT Student) <nlr3026@ad.rit.edu>
2019-11-24 15:42:44 -0500

    fixed up ball and pizza prefabs, trash randomization done but needs to be implemented

---
+++ GDD2_Project3/Assets/Scripts/Objects/TrashSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashSpawner : MonoBehaviour
+{
+    [SerializeField] List<GameObject> pref_trash;
+    [SerializeField] int numTrash = 5;
+    GameObject[] spawnpoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnpoints = GameObject.FindGameObjectsWithTag("TrashSpawnPoint");
+
+        for(int i = 0; i < numTrash; i++)
+        {
+            int trashType = Random.Range(0, pref_trash.Count);
+            int spawnpoint = Random.Range(0, spawnpoints.Length);
+
+            Instantiate(pref_trash[trashType], spawnpoints[spawnpoint].transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
