dc5d59c4b5c7621c007a63096d928ef23965d332
Kyler McQuillan <yoshi321233@gmail.com>
2019-11-25 10:55:50 -0500

    stamina work through UI

--- GDD2_Project3/Assets/Scripts/Player/PlayerStamina.cs
+++ GDD2_Project3/Assets/Scripts/Player/PlayerStamina.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.UI;
 
 public class PlayerStamina : MonoBehaviour
 {
@@ -9,6 +10,15 @@ public class PlayerStamina : MonoBehaviour
     private int time;
     private int previousTime;
     private float actualTime;
+
+    [Header("UI References")]
+    [SerializeField] public GameObject block1;
+    [SerializeField] public GameObject block2;
+    [SerializeField] public GameObject block3;
+    [SerializeField] public GameObject block4;
+    [SerializeField] public GameObject block5;
+    [SerializeField] public Sprite filled;
+    [SerializeField] public Sprite empty;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +32,7 @@ public class PlayerStamina : MonoBehaviour
     {
         actualTime += Time.deltaTime;
         UpdateStamina();
+        
 
         // if stamina is out, turn off walk
         if (stamina == 0)
@@ -36,7 +47,9 @@ public class PlayerStamina : MonoBehaviour
         //Debug.Log("Current stamina: " + stamina);
     }
 
-
+    /// <summary>
+    /// Updates stamina based on time that the player is running
+    /// </summary>
     public void UpdateStamina()
     {
         time = (int)actualTime;
@@ -50,5 +63,60 @@ public class PlayerStamina : MonoBehaviour
         }
 
         previousTime = time;
+    }
+
+    /// <summary>
+    /// Updates the UI elements for the stamina reference
+    /// </summary>
+    public void UpdateStaminaUI()
+    {
+        if (stamina == 5)
+        {
+            block1.GetComponent<Image>().sprite = filled;
+            block2.GetComponent<Image>().sprite = filled;
+            block3.GetComponent<Image>().sprite = filled;
+            block4.GetComponent<Image>().sprite = filled;
+            block5.GetComponent<Image>().sprite = filled;
+        }
+        else if (stamina == 4)
+        {
+            block1.GetComponent<Image>().sprite = filled;
+            block2.GetComponent<Image>().sprite = filled;
+            block3.GetComponent<Image>().sprite = filled;
+            block4.GetComponent<Image>().sprite = filled;
+            block5.GetComponent<Image>().sprite = empty;
+        }
+        else if (stamina == 3)
+        {
+            block1.GetComponent<Image>().sprite = filled;
+            block2.GetComponent<Image>().sprite = filled;
+            block3.GetComponent<Image>().sprite = filled;
+            block4.GetComponent<Image>().sprite = empty;
+            block5.GetComponent<Image>().sprite = empty;
+        }
+        else if (stamina == 2)
+        {
+            block1.GetComponent<Image>().sprite = filled;
+            block2.GetComponent<Image>().sprite = filled;
+            block3.GetComponent<Image>().sprite = empty;
+            block4.GetComponent<Image>().sprite = empty;
+            block5.GetComponent<Image>().sprite = empty;
+        }
+        else if (stamina == 1)
+        {
+            block1.GetComponent<Image>().sprite = filled;
+            block2.GetComponent<Image>().sprite = empty;
+            block3.GetComponent<Image>().sprite = empty;
+            block4.GetComponent<Image>().sprite = empty;
+            block5.GetComponent<Image>().sprite = empty;
+        }
+        else if (stamina == 0)
+        {
+            block1.GetComponent<Image>().sprite = empty;
+            block2.GetComponent<Image>().sprite = empty;
+            block3.GetComponent<Image>().sprite = empty;
+            block4.GetComponent<Image>().sprite = empty;
+            block5.GetComponent<Image>().sprite = empty;
+        }
     }
 }
