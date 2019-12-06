930e798a4f4ee3ad1dbf90673c1cdbbba749ffb7
alex sarnese <alexthepokemon@Gmail.com>
2019-12-06 07:34:12 -0500

    added brightness and contrast control to option menu

--- GDD2_Project3/Assets/Scripts/UI-UX/BrightnessFunctonality.cs
+++ GDD2_Project3/Assets/Scripts/UI-UX/BrightnessFunctonality.cs
@@ -2,27 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.PostProcessing;
 
 public class BrightnessFunctonality : MonoBehaviour
 {
     //render is used to change shader value for all objects using it
     float brightness = 0;
-    public Renderer rend;
+    public PostProcessingProfile ppProfile;
+    ColorGradingModel.Settings colorSettings; //temp holder of values
 
     //sets slider to shader's value
     void Start()
     {
-        float startValue = (rend.sharedMaterial.GetFloat("_Brightness") + 0.45f)/0.9f;
         Slider slider = gameObject.GetComponent<Slider>();
-        slider.normalizedValue = startValue;
-        brightness = startValue;
+        //copy current brightness settings from the profile into a temporary variable
+        colorSettings = ppProfile.colorGrading.settings;
+        slider.normalizedValue = (colorSettings.basic.postExposure+4.0f)/8.0f;
+        brightness = colorSettings.basic.postExposure;
     }
 
     //when slider is moved, send new value to shader
     public void OnValueChanged(float newValue)
     {
-        //range of -0.45f to 0.45f
-        brightness = (newValue*0.9f)-0.45f;
-        rend.sharedMaterial.SetFloat("_Brightness", brightness);
+        //range of -3f to 3f
+        brightness = (newValue*8.0f)-4.0f;
+        colorSettings = ppProfile.colorGrading.settings;
+        colorSettings.basic.postExposure = brightness;
+        ppProfile.colorGrading.settings = colorSettings;
     }
+
+    public void ResetToDefault()
+    {
+        Slider slider = gameObject.GetComponent<Slider>();
+        colorSettings = ppProfile.colorGrading.settings;
+        colorSettings.basic.postExposure = 0.0f;
+        slider.normalizedValue = (colorSettings.basic.postExposure + 4.0f) / 8.0f;
+        ppProfile.colorGrading.settings = colorSettings;
+    }
+
 }
--- GDD2_Project3/Assets/Scripts/UI-UX/ContrastFunctionality.cs
+++ GDD2_Project3/Assets/Scripts/UI-UX/ContrastFunctionality.cs
@@ -2,27 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.PostProcessing;
 
 public class ContrastFunctionality : MonoBehaviour
 {
+
     //render is used to change shader value for all objects using it
-    float contrast = 1;
-    public Renderer rend;
+    float contrast = 0;
+    public PostProcessingProfile ppProfile;
+    ColorGradingModel.Settings colorSettings; //temp holder of values
 
     //sets slider to shader's value
     void Start()
     {
-        float startValue = (rend.sharedMaterial.GetFloat("_Contrast") - 0.7f) / 2.3f;
         Slider slider = gameObject.GetComponent<Slider>();
-        slider.normalizedValue = startValue;
-        contrast = startValue;
+        //copy current bloom settings from the profile into a temporary variable
+        colorSettings = ppProfile.colorGrading.settings;
+        slider.normalizedValue = (colorSettings.basic.contrast - 0.5f) / 1.5f;
+        contrast = colorSettings.basic.contrast;
     }
 
     //when slider is moved, send new value to shader
     public void OnValueChanged(float newValue)
     {
-        //range of 0.7f to 3.0f
-        contrast = (newValue * 2.3f) + 0.7f;
-        rend.sharedMaterial.SetFloat("_Contrast", contrast);
+        //range of 0.5f to 2.0f
+        contrast = (newValue * 1.5f) + 0.5f;
+        colorSettings = ppProfile.colorGrading.settings;
+        colorSettings.basic.contrast = contrast;
+        ppProfile.colorGrading.settings = colorSettings;
+    }
+
+
+    public void ResetToDefault()
+    {
+        Slider slider = gameObject.GetComponent<Slider>();
+        colorSettings = ppProfile.colorGrading.settings;
+        colorSettings.basic.contrast = 1.0f;
+        slider.normalizedValue = (colorSettings.basic.contrast - 0.5f) / 1.5f;
+        ppProfile.colorGrading.settings = colorSettings;
     }
 }
