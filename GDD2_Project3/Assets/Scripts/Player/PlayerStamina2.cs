a76c9cc3fdea14d2b6bce000e2807531f21f1021
ryankbeach <ryankbeach@gmail.com>
2019-12-14 16:26:23 -0500

    altered ui
    made a new health prefab with hearts instead of blocks
    cleaned up the stamina2 script and made minor adjustments

--- GDD2_Project3/Assets/Scripts/Player/PlayerStamina2.cs
+++ GDD2_Project3/Assets/Scripts/Player/PlayerStamina2.cs
@@ -7,18 +7,8 @@ using UnityEngine.UI;
 public class PlayerStamina2 : MonoBehaviour
 {
     private float stamina;
-    private float time;
-    private float previousTime;
-    private float actualTime;
 
-    [Header("UI References")]
-    [SerializeField] public GameObject block1;
-    [SerializeField] public GameObject block2;
-    [SerializeField] public GameObject block3;
-    [SerializeField] public GameObject block4;
-    [SerializeField] public GameObject block5;
-    [SerializeField] public Sprite filled;
-    [SerializeField] public Sprite empty;
+    [Header("UI Reference")]
     [SerializeField] public GameObject StaminaMeter;
     private Slider staminaValue;
 
@@ -28,19 +18,18 @@ public class PlayerStamina2 : MonoBehaviour
     void Start()
     {
         stamina = 5;
-        previousTime = 0;
         staminaValue = StaminaMeter.GetComponent<Slider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        actualTime += Time.deltaTime;
         UpdateStamina();
         staminaValue.value = stamina;
+        displayStamina = stamina;
 
         // if stamina is out, turn off walk
-        if (stamina <= .1f)
+        if (stamina <= .04f)
         {
             gameObject.GetComponent<FirstPersonController>().m_RunSpeed = 1.75f;
         }
@@ -48,7 +37,7 @@ public class PlayerStamina2 : MonoBehaviour
         {
             gameObject.GetComponent<FirstPersonController>().m_RunSpeed = 3.5f;
         }
-        displayStamina = stamina;
+
         //Debug.Log("Current stamina: " + stamina);
     }
 
@@ -57,64 +46,11 @@ public class PlayerStamina2 : MonoBehaviour
     /// </summary>
     public void UpdateStamina()
     {
-        time = actualTime;
-        float diff = time - previousTime;
-        if (time != previousTime)
-        {
-            if (gameObject.GetComponent<FirstPersonController>().m_IsWalking && stamina < 5)
-                stamina += .05f;
-            else if (!gameObject.GetComponent<FirstPersonController>().m_IsWalking && stamina > 0)
-                stamina -= .05f;
-        }
-
-        previousTime = time;
-    }
-
-    /// <summary>
-    /// Updates the UI elements for the stamina reference
-    /// </summary>
-    public void UpdateStaminaUI()
-    {
-
-        if (stamina == 5)
-        {
-            block1.GetComponent<Image>().overrideSprite = filled;
-            block2.GetComponent<Image>().overrideSprite = filled;
-            block3.GetComponent<Image>().overrideSprite = filled;
-            block4.GetComponent<Image>().overrideSprite = filled;
-            block5.GetComponent<Image>().overrideSprite = filled;
-
-        }
-        else if (stamina == 4)
-        {
-            block1.GetComponent<Image>().overrideSprite = filled;
-            block2.GetComponent<Image>().overrideSprite = filled;
-            block3.GetComponent<Image>().overrideSprite = filled;
-            block4.GetComponent<Image>().overrideSprite = filled;
-            block5.GetComponent<Image>().overrideSprite = empty;
-        }
-        else if (stamina == 3)
-        {
-            block1.GetComponent<Image>().overrideSprite = filled;
-            block2.GetComponent<Image>().overrideSprite = filled;
-            block3.GetComponent<Image>().overrideSprite = filled;
-            block4.GetComponent<Image>().overrideSprite = empty;
-        }
-        else if (stamina == 2)
-        {
-            block1.GetComponent<Image>().overrideSprite = filled;
-            block2.GetComponent<Image>().overrideSprite = filled;
-            block3.GetComponent<Image>().overrideSprite = empty;
-        }
-        else if (stamina == 1)
-        {
-            block1.GetComponent<Image>().overrideSprite = filled;
-            block2.GetComponent<Image>().overrideSprite = empty;
-        }
-        else
-        {
-            block1.GetComponent<Image>().overrideSprite = empty;
-        }
+        // have preset stamina depletion and regen rates
+        if (gameObject.GetComponent<FirstPersonController>().m_IsWalking && stamina < 5)
+            stamina += .05f;
+        else if (!gameObject.GetComponent<FirstPersonController>().m_IsWalking && stamina > 0)
+            stamina -= .075f;
     }
 
     /// <summary>
