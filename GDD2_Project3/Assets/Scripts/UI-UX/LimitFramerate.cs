9ae45748c16e72cdcfd025e45607f3510e23a1ee
alex sarnese <alexthepokemon@Gmail.com>
2019-12-15 17:59:21 -0500

    fixed framerate issue

---
+++ GDD2_Project3/Assets/Scripts/UI-UX/LimitFramerate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitFramerate : MonoBehaviour
+{
+    void Awake()
+    {
+        // 0 for no sync, 1 for panel refresh rate, 2 for 1/2 panel rate
+        QualitySettings.vSyncCount = 2;
+    }
+}
